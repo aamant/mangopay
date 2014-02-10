5f6949a00fdb5c367047772494e647093498322f
Arnaud Amant <arnaud.amant@gmail.com>
2014-02-10 09:37:37 +0100

    Init

---
+++ src/Aamant/Mangopay/MangopayServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Aamant\Mangopay;
+
+use Illuminate\Support\ServiceProvider;
+
+class MangopayServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
